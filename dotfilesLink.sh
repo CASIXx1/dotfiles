45fb97242ffd6bcb9e0dc2e3a8fc841ff490c2b7
Yosuke Horikawa <uplifting0760@gmail.com>
2018-01-09 18:10:12 +0900

    [add] シンボリックリンク生成コマンド追加

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+ln -sf ~/dotfiles/.vim/rc/dein.toml ~/.vim/rc/dein.toml
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.vimrc.basic ~/.vimrc.basic
 ln -sf ~/dotfiles/colors ~/.vim
 ln -sf ~/dotfiles/ftdetect ~/.vim
 ln -sf ~/dotfiles/indent ~/.vim
