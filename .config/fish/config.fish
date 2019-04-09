a4f36b24979bb499bcdeaf7a1fdfedbdb5c5f408
Yosuke Horikawa <uplifting0760@gmail.com>
2019-04-10 01:12:11 +0900

    git push省略用の関数定義

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -172,3 +172,30 @@ end
 # these alias commit and uncomit wip branches
 alias gwip='git add -A; git ls-files --deleted -z | xargs -0 git rm; git commit -m "wip"'
 alias gunwip='git log -n 1 | grep -q -c wip; and git reset HEAD~1'
+
+function git
+  if test -z "$argv"
+    command git
+    return
+  end
+
+  switch $argv[1]
+    case push
+      set -l head
+      if not string join \n -- $argv | sed 1d | grep -E '^[^-]' >/dev/null
+        and command git status -b --porcelain=v2 | grep -E 'upstream|head' | cut -d' ' -f3 | begin
+          read head
+          and not read -l upstream
+        end
+        and command git remote get-url origin >/dev/null
+        and read -P"Set origin/$head as the upstream branch? [Y/n]: " -l ans
+        and contains "$ans" y Y ''
+
+        command git $argv -u origin $head
+      else
+        command git $argv
+      end
+    case '*'
+      command git $argv
+  end
+end
