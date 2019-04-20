58a5a114998f19b3717162bd558f5a6fe3ac6447
Yosuke Horikawa <uplifting0760@gmail.com>
2019-04-20 13:12:03 +0900

    bash系のファイル削除

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -20,7 +20,6 @@ ln -sf ~/.vimrc ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/colors ~/.vim
 ln -sf ~/dotfiles/ftdetect ~/.vim
 ln -sf ~/dotfiles/indent ~/.vim
-ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.wgetrc ~/.wgetrc
 ln -sf ~/dotfiles/Brewfile ~/Brewfile
 
