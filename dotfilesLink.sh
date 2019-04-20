ee5c71e9a4b67245f58f71ecc22bb452bb7abb70
Yosuke Horikawa <uplifting0760@gmail.com>
2019-04-20 13:01:41 +0900

    ディレクトリがない場合は、mkdirするように

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,6 +4,7 @@
 ln -sf ~/dotfiles/Brewfile ~/Brewfile
 
 # vim
+mkdir -p ~/.vim/rc
 ln -sf ~/dotfiles/.vim/rc/dein.toml ~/.vim/rc/dein.toml
 ln -sf ~/dotfiles/.vim/rc/dein_lazy.toml ~/.vim/rc/dein_lazy.toml
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
@@ -12,6 +13,7 @@ ln -sf ~/dotfiles/.vimrc.dein_load ~/.vimrc.dein_load
 ln -sf ~/dotfiles/.vimrc.nerdtree ~/.vimrc.nerdtree
 
 # neovim
+mkdir -p ~/.config/nvim
 ln -sf ~/.vim ~/.config/nvim/
 ln -sf ~/.vimrc ~/.config/nvim/init.vim
 
@@ -23,6 +25,7 @@ ln -sf ~/dotfiles/.wgetrc ~/.wgetrc
 ln -sf ~/dotfiles/Brewfile ~/Brewfile
 
 # fish
+mkdir -p ~/.config/fish
 ln -sf ~/dotfiles/.config/fish/config.fish ~/.config/fish/config.fish
 ln -sf ~/dotfiles/.config/fish/functions/git-sync.fish ~/.config/fish/functions/git-sync.fish
 ln -sf ~/dotfiles/.config/fish/functions/peco_select_ghq_repository.fish ~/.config/fish/functions/peco_select_ghq_repository.fish
