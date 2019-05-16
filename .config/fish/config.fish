8e6f6149e410d059d2050fcfc58cb13401cc1bf9
Yosuke Horikawa <uplifting0760@gmail.com>
2019-05-16 15:09:25 +0900

    peco_killのショートカット作成

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,6 +10,7 @@ status --is-interactive; and . (pyenv init - | psub)
 function fish_user_key_bindings
   bind \cf peco_select_history # Bind for peco select history to Ctrl+R
   bind \cr peco_select_ghq_repository # Bind for peco change directory to Ctrl+F
+  bind \ck peco_kill
 end
 
 # mysql
