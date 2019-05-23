8c7894dd216c29dc73d33994ae1b801547603e63
Yosuke Horikawa <uplifting0760@gmail.com>
2019-05-24 02:56:59 +0900

    標準入力の不具合修正

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,10 +6,36 @@ set -gx PYENV_ROOT "$HOME/.pyenv"
 set -x PATH $PATH "$PYENV_ROOT/bin"
 status --is-interactive; and . (pyenv init - | psub)
 
-# peco
-bind \cf peco_select_history # Bind for peco select history to Ctrl+R
-bind \cr peco_select_ghq_repository # Bind for peco change directory to Ctrl+F
-bind \ck peco_kill
+function ghq_peco_repo
+  set selected_repository (ghq list -p | peco --query "$LBUFFER")
+  if [ -n "$selected_repository" ]
+    cd $selected_repository
+    echo " $selected_repository "
+    commandline -f repaint
+  end
+end
+
+# fish key bindings
+function fish_user_key_bindings
+  bind \cg ghq_peco_repo
+  # ghq を選択
+  bind \cl peco_select_ghq_repository
+  # gh-open
+  bind \cx\cl peco_open_gh_repository
+  # コマンド履歴を見る
+  bind \cr peco_select_history
+  # プロセスをキルする
+  bind \cx\ck peco_kill
+  # 最近見たディレクトリに移動
+  bind \cx\cr peco_recentd
+
+  # fzf
+  bind \cx\cf '__fzf_find_file'
+  bind \ctr '__fzf_reverse_isearch'
+  bind \ex '__fzf_find_and_execute'
+  bind \ed '__fzf_cd'
+  bind \eD '__fzf_cd_with_hidden'
+end
 
 # mysql
 set -g fish_user_paths "/usr/local/opt/mysql@5.7/bin" $fish_user_paths
