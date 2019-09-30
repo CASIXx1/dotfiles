1dc9e7c2a543a095e5d6b41903f0bb61654efdd3
Yosuke Horikawa <uplifting0760@gmail.com>
2019-09-30 14:51:19 +0900

    cleanupの順番をupgrade後に変更

--- brew_install.sh
+++ brew_install.sh
@@ -3,15 +3,14 @@
 echo "\nbrew updating...\n"
 brew update
 
-echo "\nbrew cleanuping...\n"
-brew cleanup
-
 echo "\nbrew pin...\n"
 brew pin mysql
 
 echo "\nbrew upgrading...\n"
 brew upgrade
 
-# brew install
 echo "\nbrew installing...\n"
 brew bundle
+
+echo "\nbrew cleanuping...\n"
+brew cleanup
